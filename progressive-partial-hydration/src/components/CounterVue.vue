5a778b988a5a263afb98dd605fd66bd630ccd7a3
Julian Schäfer <schaefer@synyx.de>
2022-08-31 08:37:43 +0200

    Add example for prog. and partial hydration

---
+++ progressive-partial-hydration/src/components/CounterVue.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="counterA">
+    <button @click="subtract()">-</button>
+    <pre>{{ count }}</pre>
+    <button @click="add()">+</button>
+  </div>
+  <div class="counter-message">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const count = ref(0);
+    const add = () => (count.value = count.value + 1);
+    const subtract = () => (count.value = count.value - 1);
+
+    return {
+      count,
+      add,
+      subtract,
+    };
+  },
+};
+</script>
+
+<style>
+.counterA {
+  display: grid;
+  font-size: 2em;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  margin-top: 2em;
+  place-items: center;
+  background-color: orange;
+}
+
+.counter-message {
+  text-align: center;
+}
+</style>
---
+++ progressive-partial-hydration/src/components/StaticHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>This is a static header</header>
+</template>
+
+<script lang="ts"></script>
+
+<style>
+header {
+  font-size: 2em;
+  background-color: green;
+}
+</style>
